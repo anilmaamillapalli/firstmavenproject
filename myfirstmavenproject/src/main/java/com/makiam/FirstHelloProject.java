96319ec0fe6645be6ef41683f4d9722555b44add
makiam <makiam@makiam-PC>
2017-11-18 21:06:43 -0600

    just savd

---
+++ myfirstmavenproject/src/main/java/com/makiam/FirstHelloProject.java
@@ -0,0 +1,12 @@
+package com.makiam;
+
+import org.apache.log4j.Logger;
+
+public class FirstHelloProject {
+	static Logger log = Logger.getLogger(FirstHelloProject.class.getName());
+	public String getInfo(){
+		return "Anil Main project +anil ";
+		
+	}
+
+}
